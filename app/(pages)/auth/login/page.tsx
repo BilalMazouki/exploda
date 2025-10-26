c9496a16ccc225a8f41ca3f42b3fcd79260d926c
Bilalmaz <bilalmazouki2@gmail.com>
2025-10-26 16:45:15 +0100

    themes configured

--- app/(pages)/auth/login/page.tsx
+++ app/(pages)/auth/login/page.tsx
@@ -1,8 +1,12 @@
+import { ModeToggle } from '@/app/components/ThemeButton'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='bg-custom-100 dark:bg-custom-600 w-3xl'>
+      <ModeToggle/>
+      <h1 className='text-3xl font-bold text-custom-400 dark:text-custom-50'>Login</h1>
+    </div>
   )
 }
 
