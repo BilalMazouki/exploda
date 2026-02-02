5c58f12245bf8a2f56160dda7a0a4c44b89d40a2
Bilalmaz <bilalmazouki2@gmail.com>
2026-02-02 14:48:44 +0100

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning className={`${maxwell.variable} ${aboreto.variable}`}>
       <head>
-        <title>Mersawi Surf Experience</title>
+        <title>Exploda</title>
         <meta name="description" content="A Mersawi Surf Experience" />
         <link rel="icon" href="/logo.png" />
       </head>
